6f782d322b2a9881b4bf7812d2422102c1a96a8a
Glenn Hartmann <hartmanng@google.com>
2020-11-17 22:38:16 -0500

    Handle Web Share failure more gracefully.

--- client/components/ShareButton.js
+++ client/components/ShareButton.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ShareIcon from '@material-ui/icons/Share';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = ((canShare) => makeStyles((theme) => ({
   button: {
@@ -17,16 +19,34 @@ const useStyles = ((canShare) => makeStyles((theme) => ({
 })));
 
 export default () => {
+  const [state, setState] = useState({showSnackbar: false});
   const shareData = {url: window.location.href};
   const classes = useStyles(navigator.canShare && navigator.canShare(shareData))();
-  // TODO: handle navigator.share() failures.
-  return <Button
-      variant='contained'
-      color='primary'
-      startIcon={<ShareIcon />}
-      className={classes.button}
-      onClick={() => navigator.share(shareData)}
-    >
-      Share
-    </Button>
+
+  const share = (shareData) => {
+    navigator.share(shareData).catch((e) => {
+      if (e.name !== "AbortError") {
+        // Failure wasn't due to the user deciding to cancel the share.
+        setState({error: e.toString(), showSnackbar: true});
+      }
+    });
+  };
+
+  const closeSnackbar = () => setState({showSnackbar: false});
+  return <div>
+      <Button
+        variant='contained'
+        color='primary'
+        startIcon={<ShareIcon />}
+        className={classes.button}
+        onClick={() => share(shareData)}
+      >
+        Share
+      </Button>
+      <Snackbar open={state.showSnackbar} autoHideDuration={3000} onClose={closeSnackbar}>
+        <MuiAlert variant="filled" onClose={closeSnackbar} severity="error">
+          Failed to share: {state.error}.
+        </MuiAlert>
+      </Snackbar>
+    </div>
 };
