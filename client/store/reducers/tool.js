c405eae758476e249b4c830544c7342c1cff3ea0
Richard Knoll <richard@rknoll.at>
2021-02-08 17:46:52 +0000

    Add rainbow color easter egg (#35)
    Add rainbow color easter egg
    
    Clicking on the draw icon on the top left switches into a rainbow
    color mode. We will use this activation point for now until we
    decide that the mode is good enough to keep. If so we will move it
    to a more prominent location in the UI.

--- client/store/reducers/tool.js
+++ client/store/reducers/tool.js
@@ -8,11 +8,16 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.UPDATE_TOOL:
-      return {
+    case types.UPDATE_TOOL: {
+      const newState = {
         ...state,
         ...action.update,
       };
+      if (newState.type === 'fill' && newState.color === 'rainbow') {
+        newState.color = '#000000';
+      }
+      return newState;
+    }
     default:
       return state;
   }
